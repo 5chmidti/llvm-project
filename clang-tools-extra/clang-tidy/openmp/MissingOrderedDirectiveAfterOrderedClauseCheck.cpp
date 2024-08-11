9c7817534c7c5796bfcafa6278c88345158145b6
Julian Schmidt <git.julian.schmidt@gmail.com>
2024-08-11 21:39:57 +0200

    [clang-tidy] add static call graph support to missing-ordered-directive-after-ordered-clause

--- clang-tools-extra/clang-tidy/openmp/MissingOrderedDirectiveAfterOrderedClauseCheck.cpp
+++ clang-tools-extra/clang-tidy/openmp/MissingOrderedDirectiveAfterOrderedClauseCheck.cpp
@@ -9,9 +9,11 @@
 #include "MissingOrderedDirectiveAfterOrderedClauseCheck.h"
 #include "clang/AST/Decl.h"
 #include "clang/AST/OpenMPClause.h"
+#include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/AST/StmtOpenMP.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 #include "clang/ASTMatchers/ASTMatchers.h"
+#include <vector>
 
 using namespace clang::ast_matchers;
 
@@ -27,27 +29,113 @@ const ast_matchers::internal::VariadicDynCastAllOfMatcher<OMPClause,
 // NOLINTEND(readability-identifier-naming)
 } // namespace
 
+class Visitor : public RecursiveASTVisitor<Visitor> {
+public:
+  using Base = RecursiveASTVisitor<Visitor>;
+
+  explicit Visitor(ClangTidyCheck *Check) : Check(Check) {}
+
+  bool TraverseOMPExecutableDirective(OMPExecutableDirective *Directive) {
+    if (Directive->isStandaloneDirective())
+      return true;
+
+    const auto HasOrderedDirective =
+        Directive->getSingleClause<OMPOrderedClause>() != nullptr;
+
+    if (HasOrderedDirective)
+      Stack.push_back(false);
+    Base::TraverseStmt(Directive->getStructuredBlock());
+    if (HasOrderedDirective && !Stack.back()) {
+      Check->diag(Directive->getBeginLoc(),
+                  "this '%0' directive has an 'ordered' clause "
+                  "but the associated statement "
+                  "has no 'ordered' directive")
+          << getOpenMPDirectiveName(Directive->getDirectiveKind())
+          << Directive->getSourceRange();
+    }
+
+    return true;
+  }
+
+  bool TraverseOMPOrderedDirective(OMPOrderedDirective *Directive) {
+    if (!Stack.empty())
+      Stack.back() = true;
+
+    return true;
+  }
+
+  bool TraverseCallExpr(CallExpr *const CE) {
+    if (Stack.empty())
+      return true;
+
+    Base::TraverseStmt(CE->getCallee());
+    for (Expr *const Arg : CE->arguments())
+      Base::TraverseStmt(Arg);
+    auto *const FDecl =
+        llvm::dyn_cast_or_null<FunctionDecl>(CE->getCalleeDecl());
+    if (!FDecl)
+      return true;
+    if (llvm::is_contained(CallStack, FDecl))
+      return true;
+
+    llvm::StringRef FunctionName;
+    if (FDecl->getDeclName().isIdentifier())
+      FunctionName = FDecl->getName();
+
+    CallStack.push_back(FDecl);
+    if (FDecl->hasBody()) {
+      for (ParmVarDecl *const Param : FDecl->parameters())
+        Base::TraverseDecl(Param);
+      Base::TraverseFunctionDecl(FDecl);
+    }
+    CallStack.pop_back();
+    return true;
+  }
+
+  bool TraverseCXXOperatorCallExpr(CXXOperatorCallExpr *const CE) {
+    if (Stack.empty())
+      return true;
+
+    Base::TraverseStmt(CE->getCallee());
+    for (Expr *const Arg : CE->arguments())
+      Base::TraverseStmt(Arg);
+    auto *const FDecl =
+        llvm::dyn_cast_or_null<FunctionDecl>(CE->getCalleeDecl());
+    if (!FDecl)
+      return true;
+    if (llvm::is_contained(CallStack, FDecl))
+      return true;
+
+    llvm::StringRef FunctionName;
+    if (FDecl->getDeclName().isIdentifier())
+      FunctionName = FDecl->getName();
+
+    CallStack.push_back(FDecl);
+    if (FDecl->hasBody()) {
+      for (ParmVarDecl *const Param : FDecl->parameters())
+        Base::TraverseDecl(Param);
+      Base::TraverseFunctionDecl(FDecl);
+    }
+    CallStack.pop_back();
+    return true;
+  }
+
+  ClangTidyCheck *Check;
+  std::vector<bool> Stack{};
+  llvm::SmallVector<const FunctionDecl *> CallStack;
+};
+
 void MissingOrderedDirectiveAfterOrderedClauseCheck::registerMatchers(
     MatchFinder *Finder) {
-  Finder->addMatcher(ompExecutableDirective(
-                         hasAnyClause(ompOrderedClause()),
-                         forCallable(functionDecl().bind("context")),
-                         unless(hasDescendant(ompOrderedDirective(forCallable(
-                             functionDecl(equalsBoundNode("context")))))))
-                         .bind("dir-with-ordered"),
-                     this);
+  Finder->addMatcher(translationUnitDecl().bind("TU"), this);
 }
 
 void MissingOrderedDirectiveAfterOrderedClauseCheck::check(
     const MatchFinder::MatchResult &Result) {
-  const auto *Ordered =
-      Result.Nodes.getNodeAs<OMPExecutableDirective>("dir-with-ordered");
+  const auto *TU = Result.Nodes.getNodeAs<TranslationUnitDecl>("TU");
 
-  diag(Ordered->getBeginLoc(), "this '%0' directive has an 'ordered' clause "
-                               "but the associated statement "
-                               "has no 'ordered' directive")
-      << getOpenMPDirectiveName(Ordered->getDirectiveKind())
-      << Ordered->getSourceRange();
+  Visitor Vis{this};
+  Vis.TraverseTranslationUnitDecl(const_cast<TranslationUnitDecl *>(TU));
 }
 
 } // namespace clang::tidy::openmp
